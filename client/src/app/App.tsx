5bfd276b6f9df5b63c36bfc7154288f481de613f
dagas_sa <dagassacoop@gmail.com>
2024-09-13 21:39:45 -0400

    add: client > root.layout, router

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,9 +1,7 @@
+import RootLayout from "../components/layouts/Root.layout";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <RootLayout />;
 }
 
-export default App
+export default App;
---
+++ client/src/app/router/routes.tsx
@@ -0,0 +1,23 @@
+import { RouteObject } from 'react-router-dom';
+
+// Root
+import App from '../App';
+
+// Pages
+
+// Loaders
+
+// Interfaces
+
+// Services
+
+// const queryClient = new QueryClient();
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+  },
+];
+
+export default routes;
